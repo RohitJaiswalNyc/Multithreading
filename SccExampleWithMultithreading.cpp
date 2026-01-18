90ef0c602b59091b04f5e8b820f02e18f7bb43d6
RohitJaiswalNyc <rsjaiswa@syr.edu>
2026-01-18 16:21:15 -0500

    Move and Multithreading on custom Struct

--- SccExampleWithMultithreading.cpp
+++ SccExampleWithMultithreading.cpp
@@ -19,9 +19,8 @@
 #include <bitset>
 #include <array>
 #include<deque>
-#include <thread>
-#include <future>
-#include <sstream>
+#include<future>
+
 // hashmap
 // #include <ext/pb_ds/assoc_container.hpp>
 // using namespace __gnu_pbds;
@@ -78,166 +77,107 @@ ll oo = 2e18;
 int inf = 1e9 + 5;
 const int MOD = 998244353;
 const int N = 2e5 + 5;
-int t = 1;
-void pr(){cout << "Case #" << t << ": ";}
 
-struct Scc{
+struct arr{
+
   int n;
-  vvi adj,radj;
-  vvi comp;
-  vi tags;
-  vi st;
-  vi sz;
-  vector<bool> vis;
-  Scc(int _n,vvi&& _adj){
-    n = _n;
-    vis.resize(n);
-    tags.resize(n);
-    radj.resize(n);
-    adj = move(_adj);
+  int* a;
+
+  arr(int _n) : n(_n), a(new int[n]){}
+
+  ~arr(){
+    delete[] a;
+  }
+
+  const int& operator[](int i) const noexcept{
+    return a[i];
+  }
+  int& operator[](int i) noexcept{
+    return a[i];
+  }
+
+  arr& operator=(const arr& rhs) noexcept {
+    cout << "Copy assignment\n";
+    if(this == &rhs) return *this;
+    delete[] a;
+    this->n = rhs.n;
+    a = new int[n];
     forn(i,n){
-      for(auto v:adj[i]){
-        radj[v].pb(i);
-      }
+      a[i] = rhs[i];
     }
-  }
 
-  Scc(int _n){
-    n = _n;
-    adj.resize(n);
-    radj.resize(n);
-    tags.resize(n);
-    vis.resize(n);
-  }
+    return *this;
+  };
 
-  ~Scc(){
-    adj.clear();
-    radj.clear();
-    tags.clear();
-    vis.clear();
-    comp.clear();
-    sz.clear();
-  }
-
-  void add_edge(int u,int v){
-    adj[u].pb(v);
-    radj[v].pb(u);
-  }
-  void dfs(int u,int p){
-    for(auto v:adj[u]){
-      if(v == p || vis[v]) continue;
-      vis[v] = 1;
-      dfs(v,u);
-    }
-    st.pb(u);
-  }
-
-  void dfs2(int u,int p){
-    tags[u] = sz(comp)-1;
-    comp.back().pb(u);
-    for(auto v:radj[u]){
-      if(v == p || vis[v]) continue;
-      vis[v] = 1;
-      dfs2(v,u);
-    }
-  }
-
-  void build(){
+  arr(const arr& rhs){
+    cout << "Copy Constructor\n";
+    this->n = rhs.n;
+    a = new int[n];
     forn(i,n){
-      if(!vis[i]){
-        vis[i] = 1;
-        dfs(i,i);
-      }
-    }
-    
-    fill(all(vis),0);
-    while(!st.empty()){
-      int u = st.back();
-      st.pop_back();
-      if(vis[u]) continue;
-      comp.pb({});
-      vis[u] = 1;
-      dfs2(u,u);
-    }
-    sz.resize(sz(comp));
-    forn(i,sz(comp)){
-      sz[i] = comp[i].size();
+      a[i] = rhs[i];
     }
   }
+  
+  arr& operator=(arr&& rhs) noexcept {
+    cout << "Move assignment\n";
+    if(this == &rhs) return *this;
+    delete[] a;
 
+    this->a = rhs.a;
+    this->n = rhs.n;
+    rhs.a = nullptr;
+    rhs.n = 0;
+    return *this;
+  };
+
+  arr(arr&& rhs){
+    cout << "Move Constructor\n";
+    this->a = rhs.a;
+    this->n = rhs.n;
+
+    rhs.a = nullptr;
+    rhs.n = 0;
+  }
+
+
+
+  friend istream& operator>>(istream& i,int& v) noexcept {
+    i >> v;
+
+    return i;
+  }
+
+  int size(){
+    return this->n;
+  }
 
 };
 
-class ThreadPool {
-public:
-    ThreadPool(size_t threads) {
-        for(size_t i = 0; i < threads; ++i)
-            workers.emplace_back([this] {
-                for(;;) {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock, [this]{ return this->stop || !this->tasks.empty(); });
-                        if(this->stop && this->tasks.empty()) return;
-                        task = std::move(this->tasks.front());
-                        this->tasks.pop();
-                    }
-                    task();
-                }
-            });
-    }
-
-    template<class F, class... Args>
-    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type> {
-        using return_type = typename std::result_of<F(Args...)>::type;
-        auto task = std::make_shared<std::packaged_task<return_type()>>(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-        );
-        std::future<return_type> res = task->get_future();
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            if(stop) throw std::runtime_error("enqueue on stopped ThreadPool");
-            tasks.emplace([task](){ (*task)(); });
-        }
-        condition.notify_one();
-        return res;
-    }
-
-    ~ThreadPool() {
-        {
-            std::unique_lock<std::mutex> lock(queue_mutex);
-            stop = true;
-        }
-        condition.notify_all();
-        for(std::thread &worker: workers) worker.join();
-    }
-
-private:
-    std::vector<std::thread> workers;
-    std::queue<std::function<void()>> tasks;
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop = false;
-};
-
-
-string solve_concurrent(int test_id, int n, const vector<int>& inputs) {
-    Scc scc(n);
-    int ans(0);
-    for(int i = 0; i < n; i++) {
-        int x = inputs[i];
-        int r = (i + x + 1) % n;
-        if(i == r) ans++;
-        else scc.add_edge(i, r);
-    }
-    scc.build();
-    for(int i = 0; i < n; i++) {
-        if(scc.sz[scc.tags[i]] != 1) ans++;
-    }
-
-    return to_string(ans) + "\n";
+arr z_func(future<arr> f) {
+  arr s = f.get();
+  return s;
 }
 
+void solve(){
+  promise<arr> p;
+  future<arr> f = p.get_future();
+  future<arr> fu = async(launch::async,z_func,move(f));
+
+  arr s(10);
+  forn(i,10){
+    cin >> s[i];
+  }
+
+  
+  p.set_value(move(s));
+  arr x = fu.get();
+  cout << x.size() << endl;
+  forn(i,x.size()){
+    cout << x[i] << " ";
+  }
+  cout << endl;
+
+}
 
 int main(){
   #ifndef ONLINE_JUDGE
@@ -245,33 +185,11 @@ int main(){
     freopen("1.txt","w",stdout);  
   #endif
   FAST;
-  auto start_time = std::chrono::high_resolution_clock::now();
-  int num_tests;
-  if (!(cin >> num_tests)) return 0;
-  // Determine number of hardware threads available
-  unsigned int n_threads = std::thread::hardware_concurrency();
-  ThreadPool pool(n_threads == 0 ? 4 : n_threads); 
-
-  vector<future<string>> results;
-  results.reserve(num_tests);
-
-  for (int i = 0; i < num_tests; i++) {
-      int n; cin >> n;
-      vector<int> inputs(n);
-      for (int j = 0; j < n; j++) cin >> inputs[j];
-
-      // Queue the test case into the pool
-      results.push_back(pool.enqueue(solve_concurrent, i, n, std::move(inputs)));
-  }
-
-  // Print results as they finish (or in order)
-  for (auto& res : results) {
-      cout << res.get();
-  }
-  auto end_time = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> elapsed = end_time - start_time;
-
-  // Print the result to stderr (so it doesn't mess up your actual output files)
-  cerr << "Total runtime: " << elapsed.count() << " seconds" << endl;
+  int _ = 1;
+  cin >> _;
+  while(_--){
+    // pr();
+    solve();
+  }   
   return 0; 
 }     
