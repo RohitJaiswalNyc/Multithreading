ab03e3332249bb224eda7867dc92b373ff53c296
RohitJaiswalNyc <166319106+RohitJaiswalNyc@users.noreply.github.com>
2026-01-21 09:21:38 -0500

    Reader_writer_locking
    Implemented Reader Writer Lock Mechanism using async and thread.

---
+++ a.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+#include <array>
+#include<deque>
+#include<thread>
+#include<memory>
+#include<string>
+#include<iomanip>
+#include<shared_mutex>
+#include<future>
+// hashmap
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+using namespace std;
+ 
+#define rep(i,j,k) for(int i=j;i<=(int)k;i++)
+#define forn(i,n) for(int i=0;i<(int)n;i++)
+#define forn1(i,n) for(int i=1;i<=(int)n;i++)
+#define per(i,j,k) for(int i=j;i>=k;i--)
+#define en cout << "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+#define sz(a) (int)a.size()
+#define ins insert
+typedef long long ll;
+typedef long double ld;
+#define pii pair<int,int>
+#define pi pair<int,int>
+#define pll pair<ll,ll>
+#define plll pair<ll,pair<ll,ll>>
+#define pli pair<ll,int>
+#define vi vector<int>
+#define vl vector<ll>
+#define vvi vector<vi>
+#define vvl vector<vl>
+#define vll vector<pll>
+#define vii vector<pii>
+#define vvii vector<vii>
+#define vlll vector<plll>
+#define endl "\n"
+#define ctz(n) __builtin_ctz(n)
+#define clz(n) __builtin_clz(n)
+#define parity(n) __builtin_parity(n)
+#define popcnt(n) __builtin_popcount(n)
+#define all(x) (x).begin(), (x).end()
+#define unique(x) sort(all((x))); (x).resize(unique(all((x))) - (x).begin())
+#define MEM(x) memset(x,0,sizeof(x))
+
+// random number generater
+std::mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+#define rndm(l,r) uniform_int_distribution<int>(l,r)(rng)
+ 
+// get smaller/bigger value in a
+template<typename T, typename U> bool ckmin(T&& a,U&& b) { return b < a ? a = b, 1 : 0; }
+template<typename T,typename U> bool ckmax(T&& a,U&& b) { return a < b ? a = b, 1 : 0; }
+ 
+// constants
+ll oo = 2e18;
+int inf = 1e9 + 5;
+const int MOD = 998244353;
+const int N = 2e5 + 5;
+
+class readerWriter{
+  mutable shared_timed_mutex mtx;
+  vi data;
+  condition_variable_any cv;
+
+public:
+  int get(int i){
+    shared_lock<shared_timed_mutex> lock(mtx);
+    cv.wait(lock, [this, i] { 
+        return (int)data.size() > i; 
+    });
+    if(i > sz(data)) return -1;
+    return data[i];
+  }
+
+  void put(int val){
+    {
+        lock_guard<shared_timed_mutex> lock(mtx);
+        data.pb(val);
+    }
+    cv.notify_all();
+  }
+
+};
+
+void solve(){
+  readerWriter rw;
+  vector<future<int>> reads;
+  vector<thread> threads;
+  forn(i,11){
+    reads.pb(async(launch::async,&readerWriter::get,&rw,i));
+    threads.eb(&readerWriter::put,&rw,2*i);
+    
+  }
+  forn(i,sz(threads)){
+    threads[i].join();
+  }
+  for(future<int> &f:reads){
+    int x = f.get();
+    cout << x << " ";
+  }
+  en;
+
+}
+
+int main(){
+  #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("1.txt","w",stdout);  
+  #endif
+  FAST;
+  int _ = 1;
+  cin >> _;
+  while(_--){
+    // pr();
+    solve();
+  }   
+  return 0; 
+}     
